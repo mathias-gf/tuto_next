de190f4abd6545554ba97dc5d3cbafe3c795c0e6
XeLiOs <xelios.ytb@gmail.com>
2025-05-26 14:13:25 +0200

    test

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+
+import { Roboto, Lusitana } from "next/font/google";
+export const roboto = Roboto({ subsets: ["latin"] });
+
+export const lusitana = Lusitana({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
